95bf8e5a87f60e152b047129ee2a6f9833a1fac7
Sven <svenluecke48@gmx.de>
2020-01-20 22:31:52 +0100

    :zap: added web for inside.box

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::sync::mpsc;
 use std::thread;
+use warp::{self, Filter};
 
 mod config;
 mod input;
@@ -16,13 +17,6 @@ fn main() {
     // Create a basic channel for our main broker
     let (tx, rx) = mpsc::channel();
 
-    // Let the broker process incomming messages
-    thread::spawn(move || {
-        for msg in rx {
-            broker::broker::process_message(msg)
-        }
-    });
-
     // Spawn a new thread for waiting for inputs
     thread::spawn(move || {
         input::buzz::open_device(tx);
@@ -31,6 +25,16 @@ fn main() {
     // Blink twice to show we started successfullly
     input::buzz::blink(2, None);
 
-    //
+    // Routes for Webserving
+    let inside =
+        warp::header::exact("host", "inside.box:3000").and(warp::fs::dir("./assets/inside/"));
 
+    thread::spawn(|| {
+        warp::serve(inside).run(([127, 0, 0, 1], 3000));
+    });
+
+    // Let the broker process incomming messages
+    for msg in rx {
+        broker::broker::process_message(msg)
+    }
 }
